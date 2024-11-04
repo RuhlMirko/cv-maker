3184a553ee7dc0c7afa030fc6d069e4ca66dde9f
Mirko <ruhlmirkojoel@gmail.com>
2024-11-04 17:23:58 -0300

    Added a new multi cell paragraph maker

--- main.py
+++ main.py
@@ -10,4 +10,9 @@ pdf.add_page()
 for item in content:
     pdf.cell(h=10, text=item, new_x="LMARGIN", new_y="NEXT")
 
+paragraph = """Hi, my name is Mirko and I'm a python developer that loves to create things. I really like graphic 
+design, I have developed a lot of web and desktop apps using modern day graphics and themes, with technologies such as bootstrap 5, css, tailwind, ttkbootstrap, tkinter and pysimple."""
+
+pdf.multi_cell(0, 6, text=paragraph)
+
 pdf.output('cv.pdf')
